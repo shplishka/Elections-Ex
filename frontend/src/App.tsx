ea3c0843f9cc391946cabb718340e4e156c24e7b
michael <shplishka@gmail.com>
2020-04-16 07:27:08 +0300

    Change some routes issues

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -43,9 +43,7 @@ export default function App() {
         routes = (
             <Switch>
                 <Route path="/" exact>
-                    {/*<SignInUpWindow/>*/}
-                    <PartyResults/>
-
+                    <SignInUpWindow/>
                 </Route>
                 <Redirect to="/"/>
             </Switch>
